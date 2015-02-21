930c84ef33643508463b63f188b0b5bdbdd4481e
Kyle Peter <k.murphy.gwu@gmail.com>
2015-02-21 13:03:33 -0500

    first test push to git

---
+++ testsattest.py
@@ -0,0 +1,13 @@
+print "I'm testing my skills using Git!"
+
+print "Type a number:",
+number_one = input ()
+
+print "What number do you want to add to %r?" % (number_one),
+number_two = input ()
+
+sum_one_two = number_one+number_two
+
+print "Great! the sum is:", 
+
+print sum_one_two
